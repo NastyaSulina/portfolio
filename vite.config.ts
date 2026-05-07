0484c4476e6d1ab55443210af11dc1be4215950f
NastyaSulina <sulina01@mail.ru>
2026-05-07 18:32:50 +0300

    add deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    minify: 'esbuild',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
   },
 })
